1b19fb6901438eaa3f19f11186cd195725f39cf5
Denis Koinash <d.koinash@cyclone-nov.ru>
2024-02-20 08:35:16 +0300

    Переработка таймера

--- main.cpp
+++ main.cpp
@@ -3,30 +3,25 @@
 
 #include "myTimer.h"
 
-
 void
 TestFunc()
 {
   std::cout << " >> CallBack: Вход." << std::endl;
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  std::cout << " >> CallBack: Выход." << std::endl;
-
+  // std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  // std::cout << " >> CallBack: Выход." << std::endl;
 }
 
 int
 main(int argc, char** argv)
 {
-  Timer::Timer timer(TestFunc);
+  Timer::Timer timer;
 
-  timer.SetDuration(std::chrono::milliseconds(500));
+  timer.Start(std::chrono::milliseconds(1000), TestFunc);
+  std::cout << "Старт" << std::endl;
 
-  timer.Start();
-    
-  
+  std::this_thread::sleep_for(std::chrono::milliseconds(4000));
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(10000));
-
- timer.Stop();
+  timer.Stop();
   std::cout << "Done" << std::endl;
 
   return 0;
--- myTimer.h
+++ myTimer.h
@@ -4,28 +4,57 @@
 #include <functional>
 #include <thread>
 
-
 namespace Timer {
-using CallBackFn = std::function<void()>;
 
 class Timer
 {
 public:
-  Timer(CallBackFn callBackFn);
+  /**
+   * @brief                 Запуск таймера
+   *
+   * @tparam Rep            Количество тиков
+   * @tparam Period         Период тика
+   * @param[in] delay       Заданное время срабатывания таймера
+   * @param[in] callBackFn  Функция, вызываемая при завершении отсчёта времени таймера
+   */
+  template<class Rep, class Period>
+  void Start(const std::chrono::duration<Rep, Period>& delay, std::function<void()> callBackFn)
+  {
+    if (m_isActive) return;
+    if (delay.count() == 0) return;
+    if (callBackFn == nullptr) return;
 
-  void SetDuration(std::chrono::milliseconds value);
+    std::cout << "Start called!" << std::endl;
+    m_isActive = true;
 
-  void Start();
+    std::thread([&]() {
+      std::chrono::time_point<std::chrono::steady_clock> nextTimepoint =
+        std::chrono::steady_clock::now();
 
-  void Stop();
+      while (true) {
+        nextTimepoint += delay;
+
+        std::this_thread::sleep_until(nextTimepoint);
+        if (!m_isActive) break;
+        std::cout << "callBackFn called!" << std::endl;
+        callBackFn();
+      }
+    }).detach();
+  }
+
+  /**
+   * @brief Остановка таймера
+   */
+  void Stop() { m_isActive = false; }
+
+  /**
+   * @brief Запрос состояния таймера
+   *
+   * @return Возвращает true если таймер запущен и false в противном случае
+   */
+  bool IsActive() const { return m_isActive; }
 
 private:
-  CallBackFn m_callBackFn = nullptr;
-
-  std::chrono::milliseconds m_delay = std::chrono::milliseconds(1000);
-
-  std::chrono::time_point<std::chrono::steady_clock> m_start;
-
   bool m_isActive = false;
 };
 } // namespace Timer

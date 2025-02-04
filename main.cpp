d3fb4da51e4bb70a24a139d713d2b12dbb176b80
Denis Koinash <d.koinash@cyclone-nov.ru>
2025-02-04 17:47:24 +0300

    Рефакторинг точки входа

--- main.cpp
+++ main.cpp
@@ -6,22 +6,20 @@
 void
 TestFunc()
 {
-  std::cout << " >> CallBack: Вход." << std::endl;
-  // std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  // std::cout << " >> CallBack: Выход." << std::endl;
+  std::cout << " >> CallBack." << std::endl;
 }
 
 int
 main(int argc, char** argv)
 {
-  Timer::Timer timer;
+  Timer::AsyncTimer timer(TestFunc);
 
-  timer.Start(std::chrono::milliseconds(1000), TestFunc);
+  timer.start(std::chrono::milliseconds(800));
   std::cout << "Старт" << std::endl;
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+  std::this_thread::sleep_for(std::chrono::seconds(10));
 
-  timer.Stop();
+  timer.stop();
   std::cout << "Done" << std::endl;
 
   return 0;
